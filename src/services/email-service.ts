61b1e87444d2be0af96d434c8c26731d11523619
Tengku Hafidz <muhammad_hafidzuddin@tech.gov.sg>
2020-08-06 09:14:52 +0800

    fix spacing issue in email url

--- src/services/email-service.ts
+++ src/services/email-service.ts
@@ -1,7 +1,9 @@
+import { replaceEmptySpaces } from './utils'
+
 export const constructEmailUrl = (sendTo: string, cc: string, bcc: string, subject: string, body: string) => {
   const ccParameter: string = cc ? `cc=${cc}` : ''
   const bccParameter: string = bcc ? `&bcc=${bcc}` : ''
-  const subjectParameter: string = subject ? `&subject=${subject}` : ''
-  const bodyParameter: string = body ? `&body=${body}` : ''
+  const subjectParameter: string = subject ? `&subject=${replaceEmptySpaces(subject)}` : ''
+  const bodyParameter: string = body ? `&body=${replaceEmptySpaces(body)}` : ''
   return `mailto:${sendTo}?${ccParameter + bccParameter + subjectParameter + bodyParameter}`
 }
